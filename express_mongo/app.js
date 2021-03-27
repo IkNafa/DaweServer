e4f4d29fea81c2ae69fcbde736c2802ba4aa6acf
Iker Nafarrate <nafarrate.iker1@gmail.com>
2021-03-27 12:03:08 +0100

    .

--- express_mongo/app.js
+++ express_mongo/app.js
@@ -60,7 +60,6 @@ app.get("/", function(req, res) {
         if (err) {
             console.log(err);
         } else {
-            console.log(docs);
             res.render('index', {
                 title:'Customers',
                 users: docs,
