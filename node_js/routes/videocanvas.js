3ed582d74c3665176f8c792089724bbf0154a4f8
Iker Nafarrate <nafarrate.iker1@gmail.com>
2021-03-27 09:04:52 +0100

    commit done

--- node_js/routes/videocanvas.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const router = express.Router();
-
-router.get('/videocanvas', (req,res) => {
-    res.sendFile('/home/dawe/node_js/public/videocanvas/videocanvas.html');
-});
-
-module.exports = router;
