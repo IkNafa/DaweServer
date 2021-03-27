af033d31d64e7c5857c41830ba2bc4e5e4cfc2f2
Iker Nafarrate <nafarrate.iker1@gmail.com>
2021-03-27 11:20:16 +0100

    .

--- express_mongo/public/js/main.js
+++ express_mongo/public/js/main.js
@@ -1,16 +1,45 @@
+var mode;
+
 $(document).ready(function(){
 	$('.deleteUser').on('click', deleteUser);
 	$('.editUser').on('click', getUserForm);
+	$('[name="submit"]').on('click', addUser);
+	$('[name="edit"]').on('click', editUser);
 });
 
 function getUserForm(){
 	fetch('/users/find/'+$(this).data('id')).then(resp => resp.json()).then(resp => {
-		                console.log(resp);
-		        });
+		var user = resp[0];
+
+		$('input[name="first_name"]').val(user.first_name);
+		$('input[name="last_name"]').val(user.first_name);
+		$('input[name="email"]').val(user.first_name);
+		$('input[name="id"]').val(user._id);
+
+		mode = 1;
+
+		$('[name="edit"]').show();
+		$('[name="submit"]').hide();
+
+	});
 }
 
-function editUser(user){
-	console.log(user);
+function addUser(){
+	var formData = new FormData(document.getElementById("form"));
+	const url = '/users/add'
+	fetch(url,{
+		method: 'POST',
+		body: formData
+	})
+}
+
+function editUser(){
+	var formData = new FormData(document.getElementById("form"));
+	const url = '/users/edit'
+	fetch(url,{
+		method: 'POST',
+		body: formData
+	})
 }
 
 function deleteUser(){
