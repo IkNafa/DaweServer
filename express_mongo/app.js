effb01090b65403f115563e0da7aeb4663f636be
Iker Nafarrate <nafarrate.iker1@gmail.com>
2021-03-27 08:53:07 +0000

    Added dependencies

--- express_mongo/app.js
+++ express_mongo/app.js
@@ -121,6 +121,6 @@ app.delete('/users/delete/:id', function(req, res){
 });
 
 
-app.listen(3000, function(){
-    console.log("Servidor lanzado en el puerto 3000");
+app.listen(3002, function(){
+    console.log("Servidor lanzado en el puerto 3002");
 });
